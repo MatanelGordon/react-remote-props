bb986d0ef4f54e93b0726523e315627df7f9d8d4
Matanel Mac <mmgordon.matanel@gmail.com>
2022-11-19 20:13:37 +0200

    mid progress

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import {useState} from "react";
+import { useState } from 'react';
 
 export const Component = () => {
 	const [state] = useState(2);
-	return <div>component {state}</div>
-}
+	return <div>component {state}</div>;
+};
