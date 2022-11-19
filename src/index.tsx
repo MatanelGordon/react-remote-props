5b9633981be42f4afb0435e96f2b7c0f6f67d444
Matanel Mac <mmgordon.matanel@gmail.com>
2022-11-19 23:40:02 +0200

    added docs

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,28 @@ import { BehaviorSubject } from 'rxjs';
 export type PropsSetterParam<T> = Partial<T> | ((prev:T) => Partial<T>); 
 export type PropsSetter<T> = (value:PropsSetterParam<T>) => void;
 
-function _withProps<T>(Component: ComponentType<T>, initialProps: T): [FC<T>, PropsSetter<T>] {
-	const subject = new BehaviorSubject<T>(initialProps);
+/**
+ * creates a wrapper around a component that allows to set props remotely
+ * @param Component {ComponentType} the wanted component
+ * @param initialProps {object} the props you want to inject
+ * @returns array where the first value is the wrapped component and the second value is a function that setters the props.
+ * @example ```
+ * const [Wrapper, setProps] = withProps(Button);
+ *
+ * // as component
+ * <Wrapper />
+ *
+ * // the setter
+ * setProps({
+ *     // props to change
+ * })
+ * ```
+ */
+export function withProps<T>(Component: ComponentType<T>, initialProps: Partial<T>): [FC<T>, PropsSetter<T>] {
+	const subject = new BehaviorSubject<T>({
+		...Component.defaultProps,
+		...initialProps
+	} as T);
 
 	const setProps: PropsSetter<T> = (valueorFunc) => {
 		let nextProps;
@@ -40,30 +60,4 @@ function _withProps<T>(Component: ComponentType<T>, initialProps: T): [FC<T>, Pr
 	};
 
 	return [Wrapper, setProps];
-}
-
-/**
- * creates a wrapper around a component that allows to set props remotely
- * @param Component {ComponentType} the wanted component
- * @param partialProps {object} the props you want to inject
- * @returns array where the first value is the wrapped component and the second value is a function that setters the props.
- * @example ```
- * const [Wrapper, setProps] = withProps(Button);
- *
- * // as component
- * <Wrapper />
- *
- * // the setter
- * setProps({
- *     // props to change
- * })
- * ```
- */
-export function withProps<T>(Component:ComponentType<T>, partialProps:Partial<T>){
-	const fullProps : T = {
-		...Component.defaultProps,
-		...partialProps
-	} as T;
-
-	return _withProps(Component, fullProps);
 }
